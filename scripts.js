4a6ddefde9eaa1962d8b694128a68ffc094ab7a3
WavyBoyJodii <najeecasey@gmail.com>
2023-02-07 18:57:06 -0500

    create event listener to allow players to move used function(event) to connect each box to its place in gameboard now must fix Player.select so that if a square is taken it cannot be changed

--- scripts.js
+++ scripts.js
@@ -1,33 +1,87 @@
 const page = document.querySelector(".main");
 const gameBox = document.querySelector('.play');
 
+const Game = (() => {
+    let playerCounter = 0;
+    let turnCounter = 0;
+    let players = [];
+    const addPlayer = (x) => {
+      if (playerCounter > 1) {
+        console.log('returning');
+        return} 
+        else {
+          playerCounter++;
+          console.log(playerCounter);    
+          players.push(x);}
+        };
+        
+    const whosTurn = () => {
+      let turn;
+      if (turnCounter === 0) {
+        turn = players[0];
+        turnCounter++;}
+        else { 
+          turn = players[1];
+          turnCounter--;}
+        return turn;  
+        };
+    const makeMove = x => {
+      let currentPlayer = whosTurn();
+      currentPlayer.select(x);
+    };      
+    return {addPlayer, whosTurn, makeMove};
+})();    
+
 const Gameboard = (() => {
   const gameboard = ["X","O",,,,,,"O",,];
-  function init() {
+  const init = () => {
     for (let i=0; i<gameboard.length; i++) {
       const box = document.createElement('div');
       box.classList.add('box');
       box.textContent = gameboard[i];
       box.dataset.index = i;
-      box.addEventListener('click', Player.select)
+      box.addEventListener('click', function(event){
+        Game.makeMove(event.target.dataset.index);
+        console.log(event.target.dataset.index);
+      });
       gameBox.appendChild(box);
       }
   }
-  return {gameboard, init};
+  const removeAllChildNodes = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+  }
+  // const makeMove = x => {
+  //   let currentPlayer = Game.whosTurn();
+  //   currentPlayer.select(x);
+  // };    
+  return {gameboard, init, removeAllChildNodes};
 })();
    
-const Player = (name, symbol) => {
+const Player = (name, symbol) => { 
   const getName = () => name;
   const getSymbol = () => symbol;
-  const select = (e) => {
+  const select = (x) => {
     let choice = getSymbol();
-    let index = this.dataset.index;
-    if (gameboard[index] === "X" || "O") {
+    // let index = x;
+    if (Gameboard.gameboard[x] === 'undefined') {
+      console.log(Gameboard.gameboard[x]);  
       return} else {
-      gameboard[index] = choice;}
-    }
+      Gameboard.gameboard[x] = choice;
+      Gameboard.removeAllChildNodes(gameBox);
+      Gameboard.init();}
+    };
   return {getName, getSymbol, select};
 };
 
+const jodii = Player('Jodii', 'X');
+const eli = Player('Eli', 'O');
 
+
+Game.addPlayer(jodii);
+Game.addPlayer(eli);
 Gameboard.init();
+
+eli.select(3);
+jodii.select(8);
