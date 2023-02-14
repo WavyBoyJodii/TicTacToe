ee9847d84a989cce5f7981069ea14795cdfc94c2
WavyBoyJodii <najeecasey@gmail.com>
2023-02-13 23:53:58 -0500

    refactor code to use html game board changing code to static game board for easier javascript implementing fixed checkRoundWin function and also fixed isDraw function by changing the board to a static html board will probably have to refactor a bunch of code but in the end this will help with my problem of resetting the board after every game

--- scripts.js
+++ scripts.js
@@ -3,9 +3,23 @@ const gameBox = document.querySelector('.play');
 const infoBox = document.querySelector('.infobox');
 const startGameButton = document.createElement('button');
 const newGameButton = document.createElement('button');
+const vsPlayer1 = document.getElementById('player1');
+const vsPlayer2 = document.getElementById('player2');
+const p1wins = document.getElementById('p1Wins');
+const p2wins = document.getElementById('p2Wins');
 
 
 const Game = (() => {
+  const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
   const winnerCircle = document.createElement("div");
   winnerCircle.classList.add("winnercircle");
   let playerCounter = 0;
@@ -20,7 +34,7 @@ const Game = (() => {
       else {
         const player = Player(name, symbol);
         playerCounter++;
-        console.log(playerCounter);    
+        console.log(`this is the player counter number ${playerCounter}`);    
         players.push(player);}
       };
         
@@ -28,63 +42,71 @@ const Game = (() => {
     let turn;
     if (turnCounter === 0) {
       turn = players[0];
-      infoBox.textContent = `${players[0].getName()}'s Turn`;
+      infoBox.textContent = `${players[1].getName()}'s Turn`;
       turnCounter++;}
       else { 
         turn = players[1];
-        infoBox.textContent = `${players[1].getName()}'s Turn`;
+        infoBox.textContent = `${players[0].getName()}'s Turn`;
         turnCounter--;}
       return turn;  
       };
   const makeMove = x => {
     let currentPlayer = whosTurn();
     currentPlayer.select(x);
-    checkRoundWin();
-    checkGameWin();
+    if (checkRoundWin(currentPlayer)) {
+      endRound(false, currentPlayer)
+    } else if(isDraw()) {
+      endRound(true)
+    };
+    // checkGameWin();
   }; 
-  const checkRoundWin = () => {
-    if((Gameboard.gameboard[0] && Gameboard.gameboard[1] && Gameboard.gameboard[2]) === 'X' || (Gameboard.gameboard[0] && Gameboard.gameboard[3] && Gameboard.gameboard[6]) === 'X' || (Gameboard.gameboard[0] && Gameboard.gameboard[4] && Gameboard.gameboard[8]) === 'X' || (Gameboard.gameboard[1] && Gameboard.gameboard[4] && Gameboard.gameboard[7]) === 'X' || (Gameboard.gameboard[2] && Gameboard.gameboard[5] && Gameboard.gameboard[8]) === 'X' || (Gameboard.gameboard[2] && Gameboard.gameboard[4] && Gameboard.gameboard[6]) === 'X' || (Gameboard.gameboard[3] && Gameboard.gameboard[4] && Gameboard.gameboard[5]) === 'X' || (Gameboard.gameboard[6] && Gameboard.gameboard[7] && Gameboard.gameboard[8]) === 'X') {
-      if (players[0].getSymbol() === 'X') {
-        console.log(`${players[0].getName()} wins!`);
-        player1Wins++;
-        winnerCircle.textcontent = `${players[0].getName()} wins the round!`;
-        page.appendChild(winnerCircle);
-      } 
-      else 
-        { 
-          console.log(`${players[1].getName()} wins!`);
-          player2Wins++;
-          winnerCircle.textcontent = `${players[1].getName()} wins the round!`;
-          page.appendChild(winnerCircle);
-        }
-   } 
-    else if( (Gameboard.gameboard[0] && Gameboard.gameboard[1] && Gameboard.gameboard[2]) === 'O' || (Gameboard.gameboard[0] && Gameboard.gameboard[3] && Gameboard.gameboard[6]) === 'O' || (Gameboard.gameboard[0] && Gameboard.gameboard[4] && Gameboard.gameboard[8]) === 'O' || (Gameboard.gameboard[1] && Gameboard.gameboard[4] && Gameboard.gameboard[7]) === 'O' || (Gameboard.gameboard[2] && Gameboard.gameboard[5] && Gameboard.gameboard[8]) === 'O' || (Gameboard.gameboard[2] && Gameboard.gameboard[4] && Gameboard.gameboard[6]) === 'O' || (Gameboard.gameboard[3] && Gameboard.gameboard[4] && Gameboard.gameboard[5]) === 'O' || (Gameboard.gameboard[6] && Gameboard.gameboard[7] && Gameboard.gameboard[8]) === 'O') {
-      if (players[0].getSymbol() === 'O') {
-        console.log(`${players[0].getName()} wins the round!`);
-        player1Wins++;
-        winnerCircle.textcontent = `${players[0].getName()} wins the round!`
-        page.appendChild(winnerCircle);
-        }
-      else 
-        {
-          console.log(`${players[1].getName()} wins!`);
-          player2Wins++;
-          winnerCircle.textcontent = `${players[1].getName()} wins the round!`
-          page.appendChild(winnerCircle);
-        }
-      }
-    else if(Gameboard.gameboard.includes(undefined))
-      {
-        return
-      }
-    else 
-      { 
-        console.log('Its a Tie!');
-        winnerCircle.textcontent = `Tie Round!`
-        page.appendChild(winnerCircle);
-      };
-   };
-   
+  function checkRoundWin(currentPlayer) {
+    return WINNING_COMBINATIONS.some(combination => {
+      return combination.every(index => {
+        return Gameboard.gameboard[index] === currentPlayer.getSymbol()
+      })
+    })
+  };
+  function endRound(draw, currentPlayer) {
+    if (draw) {
+      console.log('Its a Tie!');
+      winnerCircle.textContent = `Tie Round!`;
+      page.appendChild(winnerCircle);
+      Gameboard.reset();
+      turnCounter = 0;
+      Gameboard.init();
+      infoBox.textContent = `New Round  its ${players[0].getName()}'s Turn`;
+    
+    } else {
+      console.log(`${currentPlayer.getName()} wins!`);
+      winnerCircle.textContent = `${currentPlayer.getName()} wins the round!`;
+      page.appendChild(winnerCircle);
+      whoWon(currentPlayer);
+      Gameboard.reset();
+      turnCounter = 0;
+      Gameboard.init();
+      infoBox.textContent = `New Round  its ${players[0].getName()}'s Turn`;
+    }
+  }
+  function whoWon(currentPlayer) {
+    if (currentPlayer === players[0]) {
+      player1Wins++
+    } else {
+      player2Wins++
+    }   
+  }
+  // function isDraw() {
+  //   return Gameboard.gameboard.every(index => {
+  //     return index.value === 'X' || index.value === 'O'
+  //     })
+  //   }   
+  function isDraw() {
+    if (!Gameboard.gameboard.includes('')) {
+      return true
+    } else {
+      return false
+    }
+  }
   const checkGameWin = () => {
     if( player1Wins === 5) {
       console.log(`${players[0].getName()} Wins!`);
@@ -148,6 +170,7 @@ const Game = (() => {
       for(i = 0; i < ele.length; i++) {
           if(ele[i].checked) {
           let answer = ele[i].value;
+          console.log(`this is the symbol value ${answer}`)
           return answer
           }
         }
@@ -158,6 +181,7 @@ const Game = (() => {
       for(i = 0; i < ele.length; i++) {
           if(!(ele[i].checked)) {
           let answer = ele[i].value;
+          console.log(`this is the notsymbol value ${answer}`)
           return answer
           }
         }
@@ -167,10 +191,12 @@ const Game = (() => {
       event.preventDefault();
       addPlayer(player1Name.value, symbolValue());
       console.log(player1Name.value);
+      vsPlayer1.textContent = `${player1Name.value}`;
       addPlayer(player2Name.value, notSymbolValue());
+      vsPlayer2.textContent = `${player2Name.value}`;
       infoBox.removeChild(form);
       Gameboard.init();
-      whosTurn();
+      infoBox.textContent = `${players[0].getName()}'s Turn`;
       };
         
     form.append(player1Label, player1Name, player2Label, player2Name, xSymbolLabel, xSymbol, oSymbolLabel, oSymbol, formButton);
@@ -203,14 +229,17 @@ const Game = (() => {
     player1Wins = 0;
     player2Wins = 0;
     Gameboard.gameboard = [,,,,,,,,,];
+    winnerCircle.textContent = '';
+    page.removeChild(winnerCircle);
     startGameForm();
   }  
   return {addPlayer, whosTurn, makeMove, startGame, newGame, players};
 })();    
 
 const Gameboard = (() => {
-  const gameboard = [,,,,,,,,,];
+  let gameboard = ['','','','','','','','','',];
   const init = () => {
+    removeAllChildNodes(gameBox);
     for (let i=0; i<gameboard.length; i++) {
       const box = document.createElement('div');
       box.classList.add('box');
@@ -218,7 +247,6 @@ const Gameboard = (() => {
       box.dataset.index = i;
       box.addEventListener('click', function(event){
         Game.makeMove(event.target.dataset.index);
-        console.log(event.target.dataset.index);
       });
       gameBox.appendChild(box);
       }
@@ -242,11 +270,15 @@ const Gameboard = (() => {
         parent.removeChild(parent.firstChild);
     }
   }
+  const reset = () => {
+    gameboard = [,,,,,,,,,];
+    removeAllChildNodes(gameBox);
+  }
   // const makeMove = x => {
   //   let currentPlayer = Game.whosTurn();
   //   currentPlayer.select(x);
   // };    
-  return {gameboard, init, end, removeAllChildNodes};
+  return {gameboard, init, end, reset, removeAllChildNodes};
 })();
    
 const Player = (name, symbol) => { 
